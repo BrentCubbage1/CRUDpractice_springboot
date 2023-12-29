ed2f1d4662618f873512c9deac85674f2ae73772
Brent <BrentC0328@gmail.com>
2023-12-29 13:48:50 -0500

    created personService

--- src/main/java/Brent/Cubbage/PersonControllerPractice/Service/PersonService.java
+++ src/main/java/Brent/Cubbage/PersonControllerPractice/Service/PersonService.java
@@ -1,9 +1,15 @@
 package Brent.Cubbage.PersonControllerPractice.Service;
 
 
+import Brent.Cubbage.PersonControllerPractice.Entity.Person;
 import Brent.Cubbage.PersonControllerPractice.Repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PersonService {
@@ -14,4 +20,44 @@ public class PersonService {
     public PersonService(PersonRepository repository){
         this.repository = repository;
     }
+
+
+
+    //CRUD is gonna have the services of Create, Read, ReadAll, Update, and Delete.
+
+    public Person create(Person person){
+        repository.save(person);
+        return person;
+    }
+
+
+    public Person readById(Long id){
+        return repository.findById(id).isPresent() ? repository.findById(id).get() :  null;
+    }
+
+    public List<Person> findAllPersons(){
+        Iterable<Person> personIterator = repository.findAll();
+        List<Person> personList = new ArrayList<>();
+
+        personIterator.forEach(personList::add);
+        return personList;
+    }
+
+    public Person update(Person person, Long id){
+        Person personToUpdate = readById(id);
+
+        personToUpdate.setName(person.getName());
+        personToUpdate.setAge(person.getAge());
+        repository.save(personToUpdate);
+
+        return personToUpdate;
+    }
+
+    public Person delete(Long id){
+        Person deletedPerson = readById(id);
+        repository.delete(deletedPerson);
+        return deletedPerson;
+    }
+
+
 }
