ff915f0c6c317973022583a2d30b4c9bcbe5cf95
Brent <BrentC0328@gmail.com>
2023-12-28 19:52:46 -0500

    entity and repository setup.

---
+++ src/main/java/Brent/Cubbage/PersonControllerPractice/Entity/Person.java
@@ -0,0 +1,47 @@
+package Brent.Cubbage.PersonControllerPractice.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+
+    public Person(){
+    }
+
+    public Person(String name, Integer age){
+    this.name = name;
+    this.age = age;
+
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
